44a1727edf91d0089e326c8299a596ee3b0cfe5c
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2017-10-10 21:22:25 +0200

    Added template to all tools

---
+++ src/main/scala/nl/biopet/tools/countalleles/Args.scala
@@ -0,0 +1,5 @@
+package nl.biopet.tools.countalleles
+
+import java.io.File
+
+case class Args(inputFile: File = null)
---
+++ src/test/scala/nl/biopet/tools/countalleles/CountAllelesTest.scala
@@ -0,0 +1,13 @@
+package nl.biopet.tools.countalleles
+
+import nl.biopet.test.BiopetTest
+import org.testng.annotations.Test
+
+object CountAllelesTest extends BiopetTest {
+  @Test
+  def testNoArgs(): Unit = {
+    intercept[IllegalArgumentException] {
+      ToolTemplate.main(Array())
+    }
+  }
+}
