a0ac0ac935c91337cfc52dba7be46fe54a061dca
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2017-10-11 09:34:34 +0200

    Fixed compile issue

--- src/test/scala/nl/biopet/tools/countalleles/CountAllelesTest.scala
+++ src/test/scala/nl/biopet/tools/countalleles/CountAllelesTest.scala
@@ -7,7 +7,7 @@ object CountAllelesTest extends BiopetTest {
   @Test
   def testNoArgs(): Unit = {
     intercept[IllegalArgumentException] {
-      ToolTemplate.main(Array())
+      CountAlleles.main(Array())
     }
   }
 }
