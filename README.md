84e44feeefca7a05ed6258802ea78e35bb96a9ee
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2017-12-21 10:10:31 +0100

    Auto format

--- README.md
+++ README.md
@@ -1,22 +1,36 @@
-#  CountAlleles
-This tool is part of BIOPET tool suite that is developed at LUMC by [the SASC team](http://sasc.lumc.nl/). 
+# CountAlleles
+
+
+This tool will count the alleles in multiple bam files.
+It's posible to count alleles for each separated readgroup. This can be used for quality checks.
+    
+
+# Documentation
+
+For documentation and manuals visit our [github.io page](https://biopet.github.io/countalleles).
+
+# About
+
+
+CountAlleles is part of BIOPET tool suite that is developed at LUMC by [the SASC team](http://sasc.lumc.nl/).
 Each tool in the BIOPET tool suite is meant to offer a standalone function that can be used to perform a
 dedicate data analysis task or added as part of [BIOPET pipelines](http://biopet-docs.readthedocs.io/en/latest/).
 
-#  About this tool
-CountAlleles counts the number of alleles in a VCF file. It uses a BAM file to ensure the alleles have
-a minimum quality.
+All tools in the BIOPET tool suite are [Free](https://www.gnu.org/philosophy/free-sw.html) and
+[Open Source](https://opensource.org/osd) Software.
+    
 
-#  Documentation
-For documentation and manuals visit the [readthedocs page](http://biopet-countalleles.readthedocs.io/en/latest/).
+# Contact
 
-
-#  Contact
 
 <p>
   <!-- Obscure e-mail address for spammers -->
-For any question related to this tool, please use the github issue tracker or contact 
-  <a href='http://sasc.lumc.nl/'>the SASC team</a> directly at: <a href='&#109;&#97;&#105;&#108;&#116;&#111;&#58;
- &#115;&#97;&#115;&#99;&#64;&#108;&#117;&#109;&#99;&#46;&#110;&#108;'>
-  &#115;&#97;&#115;&#99;&#64;&#108;&#117;&#109;&#99;&#46;&#110;&#108;</a>.
+For any question related to CountAlleles, please use the
+<a href='https://github.com/biopet/countalleles/issues'>github issue tracker</a>
+or contact
+ <a href='http://sasc.lumc.nl/'>the SASC team</a> directly at: <a href='&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#115;&#97;&#115;&#99;&#64;&#108;&#117;&#109;&#99;&#46;&#110;&#108;'>
+&#115;&#97;&#115;&#99;&#64;&#108;&#117;&#109;&#99;&#46;&#110;&#108;</a>.
 </p>
+
+     
+
--- src/main/scala/nl/biopet/tools/countalleles/CountAlleles.scala
+++ src/main/scala/nl/biopet/tools/countalleles/CountAlleles.scala
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2016 Sequencing Analysis Support Core - Leiden University Medical Center
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package nl.biopet.tools.countalleles
 
 import htsjdk.samtools.{SAMReadGroupRecord, SAMRecord, SamReader}
@@ -238,9 +259,24 @@ object CountAlleles extends ToolCommand[Args] {
   def exampleText: String =
     s"""
       |Default run with 1 bam file:
-      |${example("-R", "<reference_fasta>", "-I", "<input_vcf>", "-o", "<output_vcf>", "-b", "<input_bam>")}
+      |${example("-R",
+                 "<reference_fasta>",
+                 "-I",
+                 "<input_vcf>",
+                 "-o",
+                 "<output_vcf>",
+                 "-b",
+                 "<input_bam>")}
       |
       |Run with readgroups:
-      |${example("-R", "<reference_fasta>", "-I", "<input_vcf>", "-o", "<output_vcf>", "-b", "<input_bam>", "--outputReadgroups")}
+      |${example("-R",
+                 "<reference_fasta>",
+                 "-I",
+                 "<input_vcf>",
+                 "-o",
+                 "<output_vcf>",
+                 "-b",
+                 "<input_bam>",
+                 "--outputReadgroups")}
     """.stripMargin
 }
